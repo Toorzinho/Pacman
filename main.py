f2dd1b46f664aab9838604fd616445c6bc418335
ADesperateAttempt <146085981+ADesperateAttempt@users.noreply.github.com>
2023-10-02 10:41:46 +0200

    Create screen

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame
 
-#Create screen
+
+# Game start
+pygame.init
+
+
+# Create the display
+screen = pygame.display.set_mode((800,600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill((255,125,0))
+    
+    pygame.display.update
+    
