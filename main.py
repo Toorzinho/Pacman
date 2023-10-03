418e834ff43e1bb12dfa49e4eb20634d5680b8c3
Jadbinder Singh Toor <jadbindertoor@gmail.com>
2023-10-03 10:47:03 +0200

    Code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
-
+import random
+import math
+from pygame import mixer
 
 # Game start
-pygame.init
-
+pygame.init()
 
 # Create the display
 screen = pygame.display.set_mode((800,600))
@@ -12,24 +13,24 @@ screen = pygame.display.set_mode((800,600))
 playerImg = pygame.image.load('pacman.png')
 playerX = 400
 playerY = 300
+playerY_change = 0
+playerX_change = 0
 
 def player(x,y):
     screen.blit(playerImg, (x,y))
 
+# Game loop
 running = True
 while running:
+
+    # RGB = Red, Green, Blue
+    screen.fill((255,225,10))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             
-    screen.fill((255,125,0))
-    
-    if playerX <= 0:
-        playerX = 768
-    if playerX >= 768:
-        playerX = 0
-    
-    
+
     player(playerX,playerY)
     pygame.display.update
     
